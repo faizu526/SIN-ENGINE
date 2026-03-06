bc20d84b447cb72afb4407a7a96bd48173685a29
Redmoon <redmoon@Redmoon.kali>
2026-03-06 00:17:47 -0500

    Fix ratelimit.go unused import

--- backend/api-gateway/middleware/ratelimit.go
+++ backend/api-gateway/middleware/ratelimit.go
@@ -1,56 +1,55 @@
 package middleware
 
 import (
-    "net/http"
-    "sync"
-    "time"
-    
-    "github.com/gin-gonic/gin"
-    "golang.org/x/time/rate"
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
 )
 
 type IPRateLimiter struct {
-    ips    map[string]*rate.Limiter
-    mu     *sync.RWMutex
-    r      rate.Limit
-    b      int
+	ips map[string]*rate.Limiter
+	mu  *sync.RWMutex
+	r   rate.Limit
+	b   int
 }
 
 func NewIPRateLimiter(r rate.Limit, b int) *IPRateLimiter {
-    return &IPRateLimiter{
-        ips: make(map[string]*rate.Limiter),
-        mu:  &sync.RWMutex{},
-        r:   r,
-        b:   b,
-    }
+	return &IPRateLimiter{
+		ips: make(map[string]*rate.Limiter),
+		mu:  &sync.RWMutex{},
+		r:   r,
+		b:   b,
+	}
 }
 
 func (i *IPRateLimiter) GetLimiter(ip string) *rate.Limiter {
-    i.mu.Lock()
-    defer i.mu.Unlock()
-    
-    limiter, exists := i.ips[ip]
-    if !exists {
-        limiter = rate.NewLimiter(i.r, i.b)
-        i.ips[ip] = limiter
-    }
-    
-    return limiter
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	limiter, exists := i.ips[ip]
+	if !exists {
+		limiter = rate.NewLimiter(i.r, i.b)
+		i.ips[ip] = limiter
+	}
+
+	return limiter
 }
 
 var limiter = NewIPRateLimiter(10, 20) // 10 requests/sec, burst 20
 
 func RateLimiter() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        ip := c.ClientIP()
-        if !limiter.GetLimiter(ip).Allow() {
-            c.JSON(http.StatusTooManyRequests, gin.H{
-                "error": "Rate limit exceeded",
-                "retry_after": "1s",
-            })
-            c.Abort()
-            return
-        }
-        c.Next()
-    }
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		if !limiter.GetLimiter(ip).Allow() {
+			c.JSON(http.StatusTooManyRequests, gin.H{
+				"error":       "Rate limit exceeded",
+				"retry_after": "1s",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
